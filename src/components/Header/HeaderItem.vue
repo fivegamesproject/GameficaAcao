191d1a8a3fe7e45a001315cf9934d1aa26f55fc3
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2023-09-16 11:58:35 -0300

    feat: Header component stylized

---
+++ src/components/Header/HeaderItem.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  itemName: {
+    type: String,
+    required: true
+  },
+  itemLink: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <li>
+        <a href="{{ itemLink }}">{{ itemName }}</a>
+    </li>
+</template>
+
+<style scoped>
+
+li{
+  list-style: none;
+  width: fit-content;
+}
+
+a{
+  color: #FFF;
+  text-align: right;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  width: 100%;
+  height: 100%;
+}
+
+li:hover a{
+  background-color: transparent;
+  color: #b81366;
+}
+
+</style>
---
+++ src/components/Header/HeaderLogo.vue
@@ -0,0 +1,37 @@
+<script setup>
+    import IconDocumentation from './icons/IconDocumentation.vue';
+</script>
+
+<template>
+    <div id="logo">
+        <a href="#">
+            <IconDocumentation />
+            <h2>
+                GamificaAção
+            </h2>
+        </a>
+    </div>
+</template>
+
+<style scoped>
+    #logo{
+        display: flex;
+        flex-direction: row;
+        width: 200px;
+        height: auto;
+        gap: 10px;
+        align-items: center;
+        vertical-align: middle;
+    }
+
+    a{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 15px;
+    }
+
+    h2{
+        color: #fff;
+    }
+</style>
